3943e67332a107fd2e02f5fad3076056b10f9055
caorui <376176069@qq.com>
2025-06-17 23:07:18 +0800

    添加cli

--- internal/model/token.go
+++ internal/model/token.go
@@ -11,6 +11,7 @@ type Token struct {
 	URI     string // VARCHAR(256)，链接 URI（可选）
 	Creator string // VARCHAR(44)，图标（可选）
 
-	CreateAt int32 // 创建时间（blockTime 或初次发现时间）
-	UpdateAt int32 // 最后更新时间（落库时间）
+	CreateAt   int32 // 创建时间（blockTime 或初次发现时间）
+	UpdateAt   int32 // 最后更新时间（落库时间）
+	IsCreating bool
 }
